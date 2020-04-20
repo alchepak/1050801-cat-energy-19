c4541c5b41d93fba5457b742d4ba3028701853cf
alchepak <alchepak@gmail.com>
2020-04-20 23:29:27 +0800

    Исправляет скрипт меню

--- source/js/script.js
+++ source/js/script.js
@@ -2,9 +2,9 @@ var noJs = document.querySelectorAll('.no-js');
 var navToggle = document.querySelector('.page-header__toggle');
 var mainNav = document.querySelector('.main-nav');
 
-noJs.forEach(element => {
-  element.classList.remove('no-js');
-});
+for (let i = 0; i < noJs.length; i++) {
+  noJs[i].classList.remove('no-js');
+}
 
 navToggle.addEventListener('click', function() {
   this.classList.toggle('page-header__toggle--opened');
