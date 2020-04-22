4e26b6682a9d7cce98e57b8b6f8318101d044bc9
alchepak <alchepak@gmail.com>
2020-04-22 21:23:27 +0800

    Добавляет сетки элементов блока Живой пример
    Мобильная версия. Сетки, скрипт переключения слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,6 @@
-var noJs = document.querySelectorAll('.no-js');
-var navToggle = document.querySelector('.page-header__toggle');
-var mainNav = document.querySelector('.main-nav');
+let noJs = document.querySelectorAll('.no-js');
+let navToggle = document.querySelector('.page-header__toggle');
+let mainNav = document.querySelector('.main-nav');
 
 for (let i = 0; i < noJs.length; i++) {
   noJs[i].classList.remove('no-js');
@@ -10,3 +10,18 @@ navToggle.addEventListener('click', function() {
   this.classList.toggle('page-header__toggle--opened');
   mainNav.classList.toggle('main-nav--opened');
 });
+
+let changerControl = document.querySelectorAll(".changer__control");
+let changerCheckbox = document.querySelector(".changer__checkbox");
+
+for (let i = 0; i < changerControl.length; i++) {
+  changerControl[i].addEventListener('click', function() {
+    if (this.classList.contains("changer__control--after"))
+    {
+      changerCheckbox.checked = true;
+    }
+    else {
+      changerCheckbox.checked = false;
+    }
+  });
+}
