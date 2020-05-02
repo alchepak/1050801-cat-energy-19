33a598a92228fbf69d71e9efafa8d3b3f6439922
alchepak <alchepak@gmail.com>
2020-05-02 20:50:07 +0800

    Добавляет реакции на форме, обработку ввода

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,35 @@
-document.createElement( "picture" );
+/* Подключение полифиллов для IE 11 */
+
+document.createElement("picture");
 svg4everybody();
 
-let noJs = document.querySelectorAll('.no-js');
-let navToggle = document.querySelector('.page-header__toggle');
-let mainNav = document.querySelector('.main-nav');
+/* Отключение режима 'Без Javascript' */
+
+let noJs = document.querySelectorAll(".no-js");
 
 for (let i = 0; i < noJs.length; i++) {
-  noJs[i].classList.remove('no-js');
+  noJs[i].classList.remove("no-js");
 }
 
-navToggle.addEventListener('click', function() {
-  this.classList.toggle('page-header__toggle--opened');
-  mainNav.classList.toggle('main-nav--opened');
+/* Открытие и закрытие меню с помощью кнопки-гамбургера */
+
+let navToggle = document.querySelector(".page-header__toggle");
+let mainNav = document.querySelector(".main-nav");
+
+navToggle.addEventListener("click", function() {
+  this.classList.toggle("page-header__toggle--opened");
+  mainNav.classList.toggle("main-nav--opened");
+});
+
+/* Обработка некорректно заполненных полей в форме */
+
+let form = document.querySelector(".form");
+let required = form.querySelectorAll(":required");
+let submitButton = form.querySelector("[type='submit']")
+
+submitButton.addEventListener("click", function() {
+  let invalid  = form.querySelectorAll(":required:invalid");
+  for (let i = 0; i < invalid.length; i++) {
+    invalid[i].classList.add("form__invalid");
+  }
 });
